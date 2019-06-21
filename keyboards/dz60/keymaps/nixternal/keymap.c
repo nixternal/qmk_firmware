40158801790948e7c9396bcdc9ff48bac4844472
Richard A. Johnson <nixternal@gmail.com>
2019-06-21 00:01:37 -0500

    Tap Dance: Double tap Esc key for ~

--- keyboards/dz60/keymaps/nixternal/keymap.c
+++ keyboards/dz60/keymaps/nixternal/keymap.c
@@ -4,11 +4,13 @@
 
 enum {
     TD_KC_LSFT_CAPS,
-    TD_KC_RSFT_CAPS
+    TD_KC_RSFT_CAPS,
+    TD_ESC_TILDE
 };
 
 #define LShftCap TD(TD_KC_LSFT_CAPS)
 #define RShftCap TD(TD_KC_RSFT_CAPS)
+#define EscTilde TD(TD_ESC_TILDE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Default Layer
@@ -25,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *  '-----------------------------------------------------------------------------------------'
  */
 	[0] = LAYOUT_60_ansi(
-        KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,   KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,
+        EscTilde,   KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,   KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,
         KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,   KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,
         MO(1),      KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,   KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    KC_ENT,
         LShftCap,   KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,   KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    RShftCap,
@@ -83,5 +85,6 @@ void matrix_init_user(void) {
 qk_tap_dance_action_t tap_dance_actions[] = {
     // Tap shift for shift, double tap for capslock
     [TD_KC_LSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
-    [TD_KC_RSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS)
+    [TD_KC_RSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS),
+    [TD_ESC_TILDE] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_TILDE)
 };
