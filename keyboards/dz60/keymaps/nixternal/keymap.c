7c551f16b1ab5c67d3ea5196ba82f0ebec3e550b
Richard A. Johnson <nixternal@gmail.com>
2019-06-28 21:52:55 -0500

    backlighting enabled

--- keyboards/dz60/keymaps/nixternal/keymap.c
+++ keyboards/dz60/keymaps/nixternal/keymap.c
@@ -1,4 +1,10 @@
 #include QMK_KEYBOARD_H
+#include "led.h"
+
+#define _L0 0
+#define _L1 1
+#define _L2 2
+#define _L3 3
 
 #define _______ KC_TRNS
 
@@ -26,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *  | Ctrl | LWin | LAlt |                SPACE                   | RAlt | RWin | MO2  | Ctrl |
  *  '-----------------------------------------------------------------------------------------'
  */
-	[0] = LAYOUT_60_ansi(
+	[_L0] = LAYOUT_60_ansi(
         EscTilde,   KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,   KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,
         KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,   KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,
         MO(1),      KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,   KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    KC_ENT,
@@ -47,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *  |      |      |      |                                        |      |      |MO(3) |Pause |
  *  '-----------------------------------------------------------------------------------------'
  */
-	[1] = LAYOUT_60_ansi(
+	[_L1] = LAYOUT_60_ansi(
         KC_GRV,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_DEL,
         _______,    UC(0x2126), _______,    _______,    _______,    _______,    KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,     _______,    _______,    _______,    KC_INS,
         _______,    _______,    _______,    UC(0x00B0), _______,    _______,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    _______,    _______,    KC_PSCR,
@@ -68,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *  |      |      |      |                                        |      |      |      |      |
  *  '-----------------------------------------------------------------------------------------'
  */
-	[2] = LAYOUT_60_ansi(
+	[_L2] = LAYOUT_60_ansi(
         KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_VOLU,
         KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_VOLD,
         KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
@@ -89,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *  |      |      |      |                                        |      |      |      |      |
  *  '-----------------------------------------------------------------------------------------'
  */
-	[3] = LAYOUT_60_ansi(
+	[_L3] = LAYOUT_60_ansi(
         KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      EEP_RST,
         KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      RESET,
         KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
@@ -98,8 +104,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
+bool has_layer_changed = true;
+
 void matrix_init_user(void) {
     set_unicode_input_mode(UC_LNX);
+}
+
+void matrix_scan_user(void) {
+    uint8_t layer = biton32(layer_state);
+    static uint8_t old_layer = 0;
+
+    if (old_layer != layer) {
+        has_layer_changed = true;
+        old_layer = layer;
+    }
+
+    if (has_layer_changed) {
+        has_layer_changed = false;
+
+        switch (layer) {
+            case _L0:
+                backlight_set(0);
+                break;
+            case _L1:
+                backlight_set(1);
+                break;
+            case _L2:
+                backlight_set(2);
+                break;
+            case _L3:
+                backlight_set(3);
+                break;
+        }
+    }
 }
 
 /* Tap Dance Definitions */
