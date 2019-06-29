75ecc64392ed05ca87fcb4f8ffc828f4ed4f4b23
Richard A. Johnson <nixternal@gmail.com>
2019-06-28 22:54:20 -0500

    switching to grave escape, not getting used to double tapping for tilde since I use it alot for ~/

--- keyboards/dz60/keymaps/nixternal/config.h
+++ keyboards/dz60/keymaps/nixternal/config.h
@@ -1,2 +1,3 @@
 #pragma once
 #define TAPPING_TERM 200
+#define GRAVE_ESC_CTRL_OVERRIDE
--- keyboards/dz60/keymaps/nixternal/keymap.c
+++ keyboards/dz60/keymaps/nixternal/keymap.c
@@ -11,12 +11,10 @@
 enum {
     TD_KC_LSFT_CAPS,
     TD_KC_RSFT_CAPS,
-    TD_ESC_TILDE
 };
 
 #define LShftCap TD(TD_KC_LSFT_CAPS)
 #define RShftCap TD(TD_KC_RSFT_CAPS)
-#define EscTilde TD(TD_ESC_TILDE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Default Layer
@@ -33,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *  '-----------------------------------------------------------------------------------------'
  */
 	[_L0] = LAYOUT_60_ansi(
-        EscTilde,   KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,   KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,
+        KC_GESC,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,   KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,
         KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,   KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,
         MO(1),      KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,   KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    KC_ENT,
         LShftCap,   KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,   KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    RShftCap,
@@ -54,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *  '-----------------------------------------------------------------------------------------'
  */
 	[_L1] = LAYOUT_60_ansi(
-        KC_GRV,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_DEL,
+        _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_DEL,
         _______,    UC(0x2126), _______,    _______,    _______,    _______,    KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,     _______,    _______,    _______,    KC_INS,
         _______,    _______,    _______,    UC(0x00B0), _______,    _______,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    _______,    _______,    KC_PSCR,
         _______,    _______,    _______,    UC(0x00A9), _______,    _______,    _______,    KC_MUTE,    KC_VOLD,    KC_VOLU,    _______,    KC_SLCK,
@@ -144,5 +142,4 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     // Tap shift for shift, double tap for capslock
     [TD_KC_LSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
     [TD_KC_RSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS),
-    [TD_ESC_TILDE] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_TILDE)
 };
