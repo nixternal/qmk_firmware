9afeb24ff818ebf7c63d1ee677e7038443458f8f
Richard A. Johnson <nixternal@gmail.com>
2022-12-19 15:55:14 -0600

    updated to reflect new keymap stuff & it builds now without crashing

--- keyboards/dz60/keymaps/nixternal/keymap.c
+++ keyboards/dz60/keymaps/nixternal/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *  '-----------------------------------------------------------------------------------------'
  */
 	[_L0] = LAYOUT_60_ansi(
-        KC_GESC,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,   KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,
+        QK_GESC,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,   KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,
         KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,   KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,
         MO(1),      KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,   KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    KC_ENT,
         LShftCap,   KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,   KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    RShftCap,
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_DEL,
         _______,    UC(0x2126), _______,    _______,    _______,    _______,    KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,     _______,    _______,    _______,    KC_INS,
         _______,    _______,    _______,    UC(0x00B0), _______,    _______,    KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT,    _______,    _______,    KC_PSCR,
-        _______,    _______,    _______,    UC(0x00A9), _______,    _______,    _______,    KC_MUTE,    KC_VOLD,    KC_VOLU,    _______,    KC_SLCK,
+        _______,    _______,    _______,    UC(0x00A9), _______,    _______,    _______,    KC_MUTE,    KC_VOLD,    KC_VOLU,    _______,    KC_SCRL,
         _______,    _______,    _______,    _______,    _______,    _______,    MO(3),      KC_PAUS
     ),
 
@@ -92,8 +92,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *  '-----------------------------------------------------------------------------------------'
  */
 	[_L3] = LAYOUT_60_ansi(
-        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      EEP_RST,
-        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      RESET,
+        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      EE_CLR,
+        KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      QK_BOOT,
         KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
         KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
         KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      _______,    KC_NO
@@ -101,10 +101,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 bool has_layer_changed = true;
-
-void matrix_init_user(void) {
-    set_unicode_input_mode(UC_LNX);
-}
 
 void matrix_scan_user(void) {
     uint8_t layer = biton32(layer_state);
