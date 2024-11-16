5525474342eba9d8b169bbda42b8927050f4f8cc
Richard A. Johnson <nixternal@gmail.com>
2024-11-15 18:50:10 -0600

    get rid of that tap dance on shit to caps lock crap, it just gets in the damn way

--- keyboards/dz60/keymaps/nixternal/keymap.c
+++ keyboards/dz60/keymaps/nixternal/keymap.c
@@ -6,14 +6,6 @@
 #define _L2 2
 #define _L3 3
 
-enum {
-    TD_KC_LSFT_CAPS,
-    TD_KC_RSFT_CAPS,
-};
-
-#define LShftCap TD(TD_KC_LSFT_CAPS)
-#define RShftCap TD(TD_KC_RSFT_CAPS)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Default Layer
  *  ,-----------------------------------------------------------------------------------------.
@@ -32,11 +24,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         QK_GESC,    KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,   KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,
         KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,   KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,
         MO(1),      KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,   KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    KC_ENT,
-        LShftCap,   KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,   KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    RShftCap,
+        KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,   KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,
         KC_LCTL,    KC_LGUI,    KC_LALT,    KC_SPC,     KC_RALT,    KC_RGUI,    MO(2),  KC_RCTL
     ),
 
-/* FN Layer 1 (MO1) 
+/* FN Layer 1 (MO1)
  *  ,-----------------------------------------------------------------------------------------.
  *  | `~  | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |    DEL    |
  *  |-----------------------------------------------------------------------------------------+
@@ -119,10 +111,3 @@ void matrix_scan_user(void) {
             break;
     }
 }
-
-/* Tap Dance Definitions */
-tap_dance_action_t tap_dance_actions[] = {
-    // Tap shift for shift, double tap for capslock
-    [TD_KC_LSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
-    [TD_KC_RSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS),
-};
